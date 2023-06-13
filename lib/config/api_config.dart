29901453d4deb1d973a56519c0c221b81f598f9d
Christopher Hauß <christopher_hauss@gmx.de>
2023-06-13 22:16:16 +0200

    switched backendUri to localhost

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class APIConfiguration {
-  static String backendUri = "http://192.168.0.20";
-  // static String backendUri = "http://localhost";
+  // static String backendUri = "http://192.168.0.20";
+  static String backendUri = "http://localhost";
   static String backendPort = "8080";
   static String backendBaseUri = "$backendUri:$backendPort/api";
 }
