9569914f222e92191d34db007380ed9dac99e805
Christopher Hauß <christopher_hauss@gmx.de>
2023-07-21 19:39:33 +0200

    sets correct backend config

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class APIConfiguration {
-  static String backendUri = "http://192.168.0.57";
-  // static String backendUri = "http://localhost";
-  static String backendPort = "8080";
-  static String backendBaseUri = "$backendUri:$backendPort/api";
+  // static String backendUri = "http://192.168.0.57:8080";
+  // static String backendUri = "http://localhost:8080";
+  static String backendUri = "https://backend.recipy-app.de";
+  static String backendBaseUri = "$backendUri/api";
 }
